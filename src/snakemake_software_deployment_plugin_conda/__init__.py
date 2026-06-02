c5cda420934172ecc1828b7d2bb874b441d42d9d
Johannes Koester <johannes.koester@uni-due.de>
2026-06-02 21:45:46 +0200

    fix: properly handle non-local pinfiles

--- src/snakemake_software_deployment_plugin_conda/__init__.py
+++ src/snakemake_software_deployment_plugin_conda/__init__.py
@@ -67,10 +67,8 @@ class EnvSpec(EnvSpecBase):
                 "Exactly one of envfile, name, or directory must be set."
             )
 
-        if self.envfile is not None:
-            self.pinfile = EnvSpecSourceFile(
-                Path(self.envfile.path_or_uri).with_suffix(PINFILE_SUFFIX)
-            )
+        if self.envfile is not None and self.pinfile is None:
+            self.pinfile = self.envfile.replace_suffix([".yaml", ".yml"], PINFILE_SUFFIX)
 
     @classmethod
     def identity_attributes(cls) -> Iterable[str]:
